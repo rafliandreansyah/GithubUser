b4809748027e800bb5f5b8431c6c05f91889b133
rafli <rafli@mediainovasi.id>
2023-04-17 09:47:53 +0700

    Add API Service URL & UserDetailResponse

--- app/src/main/java/com/example/githubuser/data/source/remote/ApiService.kt
+++ app/src/main/java/com/example/githubuser/data/source/remote/ApiService.kt
@@ -1,6 +1,8 @@
 package com.example.githubuser.data.source.remote
 
 import com.example.githubuser.data.source.remote.entity.SearchUserResponse
+import com.example.githubuser.data.source.remote.entity.UserData
+import com.example.githubuser.data.source.remote.entity.UserDetailResponse
 import com.example.githubuser.data.source.remote.entity.UsersResponse
 import retrofit2.http.GET
 import retrofit2.http.Path
@@ -13,6 +15,15 @@ interface ApiService {
 
     @GET("/users")
     suspend fun searchUsers(@Query("q") username: String): SearchUserResponse
+
+    @GET("/users/{username}")
+    suspend fun getDetailUser(@Path("username") username: String): UserDetailResponse
+
+    @GET("/users/{username}/followers")
+    suspend fun getFollowersUser(@Path("username") username: String): List<UserData>
+
+    @GET("/users/{username}/following")
+    suspend fun getFollowingUser(@Path("username") username: String): List<UserData>
     
 
 }
